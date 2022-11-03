cfaa404b99d063e89199878bfedd982e805ad563
Bartosz Pacura <bartoszpacura@gmail.com>
2022-11-03 14:19:02 +0100

    getinfo-module-console-app

--- current-cryptocurrencies-prices.py
+++ current-cryptocurrencies-prices.py
@@ -1,55 +1,4 @@
-from requests import get
-
-BASE_URL = "https://cryptingup.com/api/assets/"
-
-
-def get_cryptocurrency(currency):
-    endpoint = currency
-    data = get(BASE_URL + endpoint).json()
-    return data
-
-
-def get_info(data):
-    data_info = data['asset']
-    currency_id = data_info['asset_id']
-    currency_name = data_info['name']
-    currency = data_info['quote']
-
-    currency_USD = currency['USD']
-    currency_price_USD = "%.2f" % round(currency_USD['price'], 2)
-
-    currency_EUR = currency['EUR']
-    currency_price_EUR = "%.2f" % round(currency_EUR['price'], 2)
-
-    currency_GBP = currency['GBP']
-    currency_price_GBP = "%.2f" % round(currency_GBP['price'], 2)
-
-    update_time = data_info['updated_at']
-
-    info = f"""
-    {'-' * 34}
-    {currency_name} ({currency_id})\n
-    USD price: {currency_price_USD}
-    EUR price: {currency_price_EUR}
-    GBP price: {currency_price_GBP}\n
-    Updated at: {update_time[:10]} {update_time[11:19]}
-    {'-' * 34}\n"""
-
-    print(info)
-
-
-def show_menu():
-    menu_text = f"""
-    {'-' * 34}
-    1. Bitcoin (BTC))
-    2. Ethereum (ETH))
-    3. Tether (USDT)
-    4. USD Coin (USDC)
-    5. BNB (BNB)
-    6. Exit
-    {'-' * 34}\n"""
-
-    print(menu_text)
+from getinfo import *
 
 
 cryptocurrencies = {
@@ -60,7 +9,7 @@ cryptocurrencies = {
     5: "BNB"}
 
 while True:
-    show_menu()
+    print(show_menu())
 
     try:
         user_choice = int(input("Enter the number to get information about the price of the cryptocurrency:\n"))
@@ -72,7 +21,7 @@ while True:
         else:
             cryptocurrency = cryptocurrencies[user_choice]
             information = get_cryptocurrency(cryptocurrency)
-            get_info(information)
+            print(get_info(information))
             input("Press Enter to continue.\n")
 
     except ValueError:
